990a9b3e6aeebeaebf885fe0de58b5d122a84a29
Hasna Fidha <hasna.fidhaah@gmail.com>
2026-08-31 12:27:48 +0530

    js Day1 completed

---
+++ Day1/index.js
@@ -0,0 +1,32 @@
+//1.student profile data
+
+let studentName = "Hasna";
+const batch = "JS";
+let age = 19;
+let course = "Mern Stack";
+
+console.log("------- Student Profile --------");
+console.log (`Name: ${studentName}`);
+console.log ("Batch:" + batch)
+console.log(`Age: ${age}`);
+
+// 2. Arithmetic operation
+let mark1 = 85;
+let mark2 = 90;
+let total = mark1 + mark2;
+let average = total / 2;
+
+console.log(`Total Marks: ${total}`);
+console.log(`Average: ${average}`);
+
+// 3.string operation
+
+let firstName = "Hasna";
+let lastName = "fidha";
+let fullName = firstName +" "+ lastName;
+console.log(`Full Name: ${fullName}`);
+console.log(`Full Name Length: ${fullName.length}`);
+
+// 4.Debugging
+
+console.log({studentName, age, course, total, average });
